c937dbc24cb0a2cf11681fe6b1bada66d5b2f233
Steven Skoczen <steven@quantumimagery.com>
2013-11-15 19:04:36 -0800

    shebang

--- fabfile.py
+++ fabfile.py
@@ -20,22 +20,8 @@ def refreeze():
     local("source ~/.virtualenvs/%(VIRTUALENV_NAME)s/bin/activate; pip install -r requirements.unstable.txt" % env)
     local("source ~/.virtualenvs/%(VIRTUALENV_NAME)s/bin/activate; pip freeze requirements.unstable.txt > requirements.txt" % env)
 
-
-def run_ve(cmd):
-    env.cmd = cmd
-    local("source ~/.virtualenvs/%(VIRTUALENV_NAME)s/bin/activate;%(cmd)s" % env)
-
-
 def deploy(target="staging"):
     env.app_string = "--app %s" % env.SERVERS[target]
-    # run_ve("export DATABASE_URL=%(DEV_DB_URL)s;./manage.py collectstatic --noinput --settings=envs.live" % env)
-    # run_ve("export DATABASE_URL=%(DEV_DB_URL)s;./manage.py compress --force --settings=envs.live" % env)
-    # run_ve("export DATABASE_URL=%(DEV_DB_URL)s;./manage.py sync_static --gzip --expires --settings=envs.live" % env)
-    deploy_code(target=target)
-
-
-def deploy_code(target="staging"):
-    env.app_string = "--app %s" % env.SERVERS[target]
-    run_ve("git push heroku master:master" % env)
-    run_ve("heroku run python manage.py syncdb --migrate --settings=envs.live %(app_string)s" % env)
-    run_ve("heroku restart %(app_string)s" % env)
+    local("git push heroku master:master" % env)
+    local("heroku run python manage.py syncdb --migrate --settings=envs.live %(app_string)s" % env)
+    local("heroku restart %(app_string)s" % env)
